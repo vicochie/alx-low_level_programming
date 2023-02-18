f717afdd44736a196139c9ce0fbc98dd4c3dccde
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-17 17:14:56 -0800

    Three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 {
 int i, j, k;
 
-for (i = 0; i <= 7; i++)
-{    
-for (j = i + 1; j <= 8; j++)
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
 {
 for (k = j + 1; k <= 9; k++)
 {
