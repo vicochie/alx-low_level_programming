10f4b4d909fc33229390f4d816c12647016365e6
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-20 13:04:22 -0800

    12.Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - Entry point for a program that prints all possible combinations
+ * of two two-digit numbers.
+ * The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits. 1 be printed as 01
+ * Combination of numbers must be separated by comma, followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * only use putchar eight times maximum in your code
+ * No use of char variables
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j;
+	int f, s;
 
-for (i = 0; i <= 99; i++)
-{
-for (j = i + 1; j <= 99; j++)
-{
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
+	for (f = 0; f <= 99; f++)
+	{
+		for (s = f + 1; s <= 99; s++)
+		{
+			putchar(f / 10 + '0');
+			putchar(f % 10 + '0');
+			putchar(' ');
+			putchar(s / 10 + '0');
+			putchar(s % 10 + '0');
 
-if (i < 98 || j < 99)
-{
-putchar(',');
-putchar(' ');
+			if (f < 98 || s < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
 }
-}
-}
-
-putchar('\n');
-
-return (0);
-}
-
