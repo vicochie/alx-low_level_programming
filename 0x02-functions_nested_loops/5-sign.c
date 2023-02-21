51e27dee268aca3418fa0239459565a47010c763
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-21 15:38:43 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - this program prints the sign of a number
+ * @n: Number checked
+ *
+ * Return: 1 if n is greater than zero
+ * 0 if n is zero , -1 if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
