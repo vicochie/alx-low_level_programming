d80defadb3c788c382f993a92671731e81f05704
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-21 14:17:57 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x01(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalphabet(int c);
 int print_sign(int n);
