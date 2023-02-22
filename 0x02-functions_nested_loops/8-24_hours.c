febf8d20bb81359b72d175f4b67f8614b134d51a
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-22 01:18:06 -0800

    8.I am a federal agent

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - A function that prints every minute of the day of Jack
+ */
+
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar(hrs / 10 + '0');
+			_putchar(hrs % 10 + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(mins % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
