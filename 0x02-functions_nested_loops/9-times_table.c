9db55acce01af4c1c9062d5c5fab015dcc4b6e78
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-22 01:51:34 -0800

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - A function that prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j <= 9; j++)
+		{
+			res = i * j;
+
+			if (res <= 9)
+			{
+				_putchar(' ');
+				_putchar(res + '0');
+			}
+			else
+			{
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
