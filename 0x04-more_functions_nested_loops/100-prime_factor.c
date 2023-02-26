c569744d5128370cc343b5df1e0ab0aaae6574bc
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-25 16:21:12 -0800

    11. The problem of distinguishing prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	long n = 612852475143;
+	long i;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			i--;
+		}
+	}
+
+	printf("%ld\n", i);
+
+	return (0);
+}
