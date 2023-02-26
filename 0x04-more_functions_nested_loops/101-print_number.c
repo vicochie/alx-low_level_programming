20a98c3e8310b9b90ef3e678faaed545569727cd
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-25 16:42:59 -0800

    12.Numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('_');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+}
