b0aad32e67abf622f822c18a82abd808ba8e6d61
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-27 14:01:14 -0800

    6.Half the lies they tell

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Prinnts every other caharacter of a string.
+ * @str: A pointer to the stro=ing to print
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
