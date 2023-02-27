daff6d29e91dfa30b51f855a0b0c30374bdbd0f4
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-27 12:45:23 -0800

    2.This report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to a string.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
