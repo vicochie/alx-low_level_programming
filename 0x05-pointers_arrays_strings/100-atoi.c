9cee825ef89e5c052d1d1aedc0cccdf60c0afc63
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-28 02:12:01 -0800

    10. Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,24 +10,47 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
 	{
-		if (s[i] == '-')
+		len++;
+	}
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '_')
 		{
-			sign *= -1;
+			++d;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+		}
+		if (d % 2)
+		{
+			digit = -digit;
+		}
+		n = n * 10 + digit;
+		f = 1;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+		{
+			break;
 		}
 		i++;
 	}
 
-	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	if (f == 0)
 	{
-		result = result * 10 + (s[i] - '0');
-		i++;
+		return (0);
 	}
-
-	return (sign * result);
+	return (n);
 }
