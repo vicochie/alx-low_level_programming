dfd031b8cdc2dfb9a1dacd28ac41d3208244588a
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-02-28 01:03:25 -0800

    6. Half the lies they tell about me

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,23 @@
 
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (*y != '\0')
 	{
-		_putchar(str[i]);
+		y++;
+		longi++;
 	}
-
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
 	_putchar('\n');
 }
