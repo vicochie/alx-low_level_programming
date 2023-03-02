aa9f3a1a956bf1047d37e4f8a1a8deed31534739
Victor Otieno Ochieng <ochiengv806@gmail.com>
2023-03-02 15:06:05 -0800

    10. A dream

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	printf("a[2] = %d\n", *(p + (3 * 4 - 14)) = 98);
+	*(a + 2) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
